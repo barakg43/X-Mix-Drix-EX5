5aeff96f921fa43de4d9885206028ead64c6860a
barakg43 <93736137+barakg43@users.noreply.github.com>
2023-05-11 12:04:33 +0300

    add game logic class

---
+++ Ex02/ePlayerName.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex02
+{
+    public enum ePlayerName
+    {
+        Player1,
+        Player2,
+        Computer
+    }
+}
