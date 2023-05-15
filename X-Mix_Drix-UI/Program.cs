4c0c3fdedacd90e03013684a09bbe7077ae87f37
Yoav Schwartz <yoavsc@mta.ac.il>
2023-05-15 16:58:05 +0300

    Refactor Code

--- X-Mix_Drix-UI/Program.cs
+++ X-Mix_Drix-UI/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Ex02.ConsoleUtils;
-namespace X_Mix_Drix_UI
+﻿namespace X_Mix_Drix_UI
 {
     class Program
     {
